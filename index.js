1fa0c9611b36b559cf0858ebf758bad51054c566
tannguyenk3 <jusstan111@gmail.com>
2024-08-02 17:00:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const containerEl = document.getElementById('container')
+const leftEl = document.getElementById('left-side')
+const rightEl = document.getElementById('right-side')
+
+const userEmail = document.getElementById('user-email')
+const invalidEmail = document.getElementById('invalid-email')
+const emailInput = document.getElementById('email')
+const submitBtn = document.getElementById('submit-btn')
+
+const  confirmMessage = document.getElementById('confirmed-message')
+const dismissMessage = document.getElementById('dismiss-message')
+
+function formSuccess() {
+    confirmMessage.classList.add('active')
+    containerEl.classList.add('success')
+    leftEl.style.display = 'none'
+    rightEl.style.display = 'none'
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(email)
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const email = emailInput.value.trim()
+    
+    if(validateEmail(email)) {
+        formSuccess()
+        userEmail.innerText = email
+        emailInput.value = ''
+
+        invalidEmail.classList.remove('active')
+        emailInput.classList.remove('active')
+    } else {
+        invalidEmail.classList.add('active')
+        emailInput.classList.add('active')
+    }
+})
+
+dismissMessage.addEventListener('click', () => {
+    confirmMessage.classList.remove('active')
+    containerEl.classList.remove('success')
+    leftEl.style.display = 'block'
+    rightEl.style.display = 'block'
+})
